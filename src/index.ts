bee6e79edbd4a3fca34422b80661735ea507920e
Flamur Mavraj <flamur.mavraj@ornio.no>
2020-05-31 23:08:30 +0200

    Install and setup Express.js

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+// Boot express
+const app: Application = express();
+const port = 5000;
+
+// Application routing
+app.use('/', (req: Request, res: Response, next: NextFunction ) => {
+    res.status(200).send({data: 'Hello from Ornio AS'});
+});
+
+// Start server
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
